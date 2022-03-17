16cb1ba56e38b83ab66b664e100d363ff6a4b144
Tomer Groisman <tomergroisman@gmail.com>
2022-03-17 12:06:43 +0200

    Export InterceptionHandler

--- index.d.ts
+++ index.d.ts
@@ -3,4 +3,5 @@ import { Netmock } from './src/types/netmock';
 declare const netmock: Netmock;
 export default netmock;
 
+export { InterceptionHandler } from './src/types/interceptor';
 export { Method, NetmockRequest, NetmockResponse } from './src/types/base';
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+const { InterceptionHandler } = require('./src/types/interceptor');
 const {
   Method,
   NetmockRequest,
   NetmockResponse,
 } = require('./src/types/base');
+
 const netmock = require('./src/classes/netmock');
 
 module.exports = netmock;
 
 Object.assign(module.exports, {
+  InterceptionHandler,
   Method,
   NetmockRequest,
   NetmockResponse,
