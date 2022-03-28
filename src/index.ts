1b1e9d1011b83a142d5e2a49fe5fdd1d525abfb3
Nir Yosef <niryo@wix.com>
2022-03-28 14:40:16 +0300

    expose NetmockResponse

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@ import { registerMockedEndpoint } from './mockedEndpointsService';
 import { MockedEndpointHandler, Netmock } from './types';
 
 export { allowRealNetwork } from './settings';
-export { resp } from './NetmockResponse';
+export { resp, NetmockResponse } from './NetmockResponse';
 export * from './types';
-
 export const netmock: Netmock = {
   get: (url: string | RegExp, handler: MockedEndpointHandler) => registerMockedEndpoint('get', url, handler),
   post: (url: string | RegExp, handler: MockedEndpointHandler) => registerMockedEndpoint('post', url, handler),
