bd8d5a7901dee878b20f7ea5d12b841aac633410
Nir Yosef <niryo@wix.com>
2022-07-26 18:49:01 +0300

    inline axios fetch adapter

--- src/jest-setup.ts
+++ src/jest-setup.ts
@@ -4,7 +4,7 @@ import { allowRealNetwork } from './settings';
 import { overrideFetch } from './overrideFetch';
 
 beforeEach(() => {
-  require('axios').defaults.adapter = require('@vespaiach/axios-fetch-adapter').default;
+  require('axios').defaults.adapter = require('./axios-fetch-adapter').default;
 
   overrideFetch();
   allowRealNetwork(false);
