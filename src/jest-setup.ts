9b209b8f2b2dd6d0b59e602be11cffbe52ef20f6
Nir Yosef <niryo@wix.com>
2022-07-27 19:41:05 +0300

    make sure axios is singleton

--- src/jest-setup.ts
+++ src/jest-setup.ts
@@ -3,11 +3,23 @@ import { reset } from './mockedEndpointsService';
 import { allowRealNetwork } from './settings';
 import { overrideFetch } from './overrideFetch';
 
-beforeEach(() => {
-  require('axios').defaults.adapter = require('./axios-fetch-adapter').default;
+// make sure that axios is a singleton
+let actualAxios: any;
+jest.doMock('axios', () => {
+  if (!actualAxios) {
+    actualAxios = jest.requireActual('axios');
+  }
+  return actualAxios;
+}, { virtual: true });
 
+beforeEach(() => {
   overrideFetch();
   allowRealNetwork(false);
+  try {
+    require('axios').defaults.adapter = require('./axios-fetch-adapter').default;
+  } catch {
+    // no need to do anythinng if axios isn't available.
+  }
 });
 
 afterEach(() => {
