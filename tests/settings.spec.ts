3f4cabc01c7c19c9f8f4cdabf3970c4976345281
Nir Yosef <niryo@wix.com>
2023-04-16 15:57:33 +0300

    more code review fixes

--- tests/settings.spec.ts
+++ tests/settings.spec.ts
@@ -9,21 +9,13 @@ describe('Settings', () => {
 
   it('should make a real network call if network is enabled and an unmocked request is fetched', async () => {
     require('netmock-js').allowRealNetwork(true);
-    await expect(async () => {
-      await fetch('https://this-site-does-not-exist-niryo.com');
-    }).rejects.toThrowError('ENOTFOUND');
-
-    await expect(async () => {
-      await axios.get('https://this-site-does-not-exist-niryo.com');
-    }).rejects.toThrowError('ENOTFOUND');
+    await expect(fetch('https://this-site-does-not-exist-niryo.com')).rejects.toThrowError('ENOTFOUND');
+    await expect(axios.get('https://this-site-does-not-exist-niryo.com')).rejects.toThrowError('ENOTFOUND');
   });
 
   it('should allow real network to specific url pattern', async () => {
     require('netmock-js').allowRealNetwork(/this-site-does-not-exist-niryo/);
-    await expect(async () => {
-      await fetch('https://this-site-does-not-exist-niryo.com');
-    }).rejects.toThrowError('ENOTFOUND');
-
+    await expect(fetch('https://this-site-does-not-exist-niryo.com')).rejects.toThrowError('ENOTFOUND');
     await expect(fetch('https://wix.com')).rejects.toThrowError('Endpoint not mocked: GET https://wix.com');
     await expect(axios.get('https://this-site-does-not-exist-niryo.com')).rejects.toThrowError('ENOTFOUND');
     await expect(axios.get('https://wix.com')).rejects.toThrowError('Endpoint not mocked: GET https://wix.com');
