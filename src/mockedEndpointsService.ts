44ed74599e1226697171b8aff57d65716e998ed3
Nir Yosef <niryo@wix.com>
2023-04-16 16:16:22 +0300

    validate no query params (#20)

--- src/mockedEndpointsService.ts
+++ src/mockedEndpointsService.ts
@@ -29,6 +29,9 @@ export function getMockedEndpointMetadata(method: Method, url: MockedUrl) {
 }
 
 export function registerMockedEndpoint(method: Method, url: MockedUrl, handler: MockedEndpointHandler, stackTrace: string) {
+  if (typeof url === 'string' && isContainingQueryParams(url)) {
+    throw new Error(`Cannot mock endpoint with query params. Please remove the query params from the url. Endpoint: ${url}`);
+  }
   const key = getMockedEndpointKey(url);
   const urlRegex = url instanceof RegExp ? url : convertUrlToRegex(url);
   const metadata = getEmptyMetadata();
@@ -68,4 +71,8 @@ function getEmptyMetadata(): MockedEndpointMetaData {
   return {
     calls: [],
   };
+}
+
+function isContainingQueryParams(url: string) {
+  return url.includes('?');
 }
